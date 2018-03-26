b950f866d6f8c783549968897afee558b801091f
rohith-vallabhaneni <rohithchowdaryvallabhaneni@gmail.com>
2018-03-26 14:54:49 +0200

    Update ValuesController.cs

--- demo/Controllers/ValuesController.cs
+++ demo/Controllers/ValuesController.cs
@@ -13,7 +13,7 @@ namespace demo.Controllers
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "Hello", "Rohith Vallabhaneni" };
         }
 
         // GET api/values/5
